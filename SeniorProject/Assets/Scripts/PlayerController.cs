9061e48bd1b9d7920a5ae1e33d8d6c5505b839e3
Shean92 <sheanthebond92@gmail.com>
2022-04-21 18:26:41 -0700

    Adding Movement Script

--- SeniorProject/Assets/Scripts/PlayerController.cs
+++ SeniorProject/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed;
+    public float health;
 
-    // Update is called once per frame
-    void Update()
+    public Rigidbody2D rb;
+    public Camera cam;
+    private Vector2 movement;
+    private Vector2 mousePos;
+
+    private void Update()
     {
-        
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
