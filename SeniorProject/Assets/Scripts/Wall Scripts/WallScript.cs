df298acc74f246273370ad28df7666532a1dd8e1
Avery Crowson <crowson.avery@gmail.com>
2022-06-26 15:51:31 -0600

    map

---
+++ SeniorProject/Assets/Scripts/Wall Scripts/WallScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameManager game; 
+    public GameObject wall;
+
+
+    private void Start()
+    {
+      game = GameObject.FindGameObjectWithTag("GameManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (game.enemyKillCount >= 3)
+        {
+            Destroy(gameObject);
+        }
+            
+    }
+}
